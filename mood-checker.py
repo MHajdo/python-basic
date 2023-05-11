4f28ea4cf3e1f4159e8d92f80495df1ef70bd91b
Matej Hajdinjak <hajdinjak.matej@gmail.com>
2023-05-11 19:28:07 +0200

    mood checker

---
+++ mood-checker.py
@@ -0,0 +1,22 @@
+# Mood checker
+
+mood = input("What mood are you in: ")
+
+if mood == "happy":
+    print('It is great to see you happy!')
+elif mood == "nervous":
+    print('Take a deep breath 3 times.')
+elif mood == "sad":
+    print('Cheer up')
+elif mood == "excited":
+    print('Woohooo!')
+elif mood == "relaxed":
+    print('keep chilling')
+else:
+    print("I don't recognize this mood")
+
+
+
+
+
+
