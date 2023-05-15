777b2e0b30a4f0e1a9c64759babd55bc8572e91b
Matej Hajdinjak <hajdinjak.matej@gmail.com>
2023-05-15 19:55:00 +0200

    added while loop

--- secret-number.py
+++ secret-number.py
@@ -2,9 +2,15 @@
 
 secret = 19
 
-guess = int(input('Guess the number: '))
+guess = None
 
-if secret == guess:
-    print("Congratulations! Your guess is correct.")
-else:
-    print("Too bad, " + str(guess) + " is not the secret number.")
+
+while guess != secret:
+    guess = int(input('Guess the number: '))
+
+    if secret == guess:
+        print("Congratulations! Your guess is correct.")
+    else:
+        print("Too bad, this is not the secret number.")
+
+print('Game over')
