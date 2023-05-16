bb4ea77c3ba972d0a5bd9b8668f0e425d3a2d279
Matej Hajdinjak <hajdinjak.matej@gmail.com>
2023-05-16 17:04:56 +0200

    fixed range (starts counting from 1), added stylistic changes

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,12 @@
 num = int(input("Enter a number between 1 and 100: "))
 
-for x in range(num + 1):
+for x in range(1, num + 1):
 
-
-    if ((x % 5 == 0) and (x % 3 == 0) and (x >= 5)):
+    if x % 5 == 0 and x % 3 == 0:
         print("fizzbuzz")
-    elif (x % 5 == 0) and (x >= 5):
+    elif x % 5 == 0:
         print("buzz")
-    elif (x % 3 == 0) and (x >= 3):
+    elif x % 3 == 0:
         print("fizz")
     else:
         print(f"{x}")
