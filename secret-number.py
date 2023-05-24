61fdf4ba77aae2a2bcc52843d90e19fcb598a721
Matej Hajdinjak <hajdinjak.matej@gmail.com>
2023-05-24 21:15:02 +0200

    added unsuccessful guesses

--- secret-number.py
+++ secret-number.py
@@ -9,10 +9,12 @@ with open("resources/score_list.json") as score_file:
     score_list = json.loads(score_file.read())
 
 for score_dict in score_list:
-    print("Best scores: " + score_dict["Name"] + ", " + str(score_dict["attempts"]) + " attempts, date: " + str(score_dict["date"]))
+    print(f'Best scores: {score_dict["Name"]}, {score_dict["attempts"]} attempts, date: {score_dict["date"]} '
+          f'unsuccessful guesses: {score_dict.get("wrong_guesses")}')
 
 username = input("Enter your name: ")
 
+wrong_guesses = []
 while True:
     guess = int(input('Guess the number (from 1 - 45): '))
     attempts += 1
@@ -21,14 +23,17 @@ while True:
         print(f"Congratulations! {guess} is the secret number!")
         print(f"You needed {attempts} attempts.")
 
-        score_list.append({"Name": username, "attempts": attempts, "date": str(datetime.datetime.now())})
+        score_list.append({"Name": username, "attempts": attempts, "date": str(datetime.datetime.now()),
+                           "wrong_guesses": wrong_guesses})
 
         with open("resources/score_list.json", "w") as score_file:
             score_file.write(json.dumps(score_list))
         break
     elif secret > guess:
+        wrong_guesses.append(guess)
         print("The secret number is bigger. Try again.")
     else:
+        wrong_guesses.append(guess)
         print("The secret number is smaller. Try Again")
 
 print('Game over')
