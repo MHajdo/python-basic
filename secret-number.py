2e8e84b759061cf445161a05ee0c70b70df7cf0d
Matej Hajdinjak <hajdinjak.matej@gmail.com>
2023-05-24 20:53:48 +0200

    added username and date

--- secret-number.py
+++ secret-number.py
@@ -1,12 +1,17 @@
 import json
 import random
+import datetime
 
 attempts = 0
 secret = random.randint(1, 45)
 
 with open("resources/score_list.json") as score_file:
     score_list = json.loads(score_file.read())
-    print(f"Best score: {score_list}")
+
+for score_dict in score_list:
+    print("Best scores: " + score_dict["Name"] + ", " + str(score_dict["attempts"]) + " attempts, date: " + str(score_dict["date"]))
+
+username = input("Enter your name: ")
 
 while True:
     guess = int(input('Guess the number (from 1 - 45): '))
@@ -15,7 +20,9 @@ while True:
     if secret == guess:
         print(f"Congratulations! {guess} is the secret number!")
         print(f"You needed {attempts} attempts.")
-        score_list.append(attempts)
+
+        score_list.append({"Name": username, "attempts": attempts, "date": str(datetime.datetime.now())})
+
         with open("resources/score_list.json", "w") as score_file:
             score_file.write(json.dumps(score_list))
         break
