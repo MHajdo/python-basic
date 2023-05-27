844abca8133a98b6d8bebd8a2fa5922d79e9dc0b
Matej Hajdinjak <hajdinjak.matej@gmail.com>
2023-05-27 11:03:55 +0200

    added functions to make a game more modular

--- secret-number.py
+++ secret-number.py
@@ -2,40 +2,59 @@ import json
 import random
 import datetime
 
-attempts = 0
-secret = random.randint(1, 45)
 
-with open("resources/score_list.json") as score_file:
-    score_list = json.loads(score_file.read())
+def get_score_list():
+    with open("resources/score_list.json") as score_file:
+        score_list = json.loads(score_file.read())
+        return score_list
 
-score_list.sort(key=lambda d: d['attempts'])
 
-for score_dict in score_list[:3]:
-    print(f'Best scores: {score_dict["Name"]}, {score_dict["attempts"]} attempts, date: {score_dict["date"]} '
-          f'unsuccessful guesses: {score_dict.get("wrong_guesses")}')
+def play_game():
+    attempts = 0
+    secret = random.randint(1, 45)
+    score_list = get_score_list()
 
-username = input("Enter your name: ")
+    username = input("Enter your name: ")
 
-wrong_guesses = []
+    wrong_guesses = []
+    while True:
+        guess = int(input('Guess the number (from 1 - 45): '))
+        attempts += 1
+
+        if secret == guess:
+            print(f"Congratulations! {guess} is the secret number!")
+            print(f"You needed {attempts} attempts.")
+
+            score_list.append({"Name": username, "attempts": attempts, "date": str(datetime.datetime.now()),
+                               "wrong_guesses": wrong_guesses})
+
+            with open("resources/score_list.json", "w") as score_file:
+                score_file.write(json.dumps(score_list))
+            break
+        elif secret > guess:
+            wrong_guesses.append(guess)
+            print("The secret number is bigger. Try again.")
+        else:
+            wrong_guesses.append(guess)
+            print("The secret number is smaller. Try Again")
+
+
+def get_top_score():
+    score_list = get_score_list()
+    score_list.sort(key=lambda d: d['attempts'])
+
+    for score_dict in score_list[:3]:
+        print(f'Best scores: {score_dict["Name"]}, {score_dict["attempts"]} attempts, date: {score_dict["date"]} '
+              f'unsuccessful guesses: {score_dict.get("wrong_guesses")}')
+
+
 while True:
-    guess = int(input('Guess the number (from 1 - 45): '))
-    attempts += 1
+    selection = input("Would you like to: \nA) play a new game, \nB) see the best scores, \nC) quit?")
 
-    if secret == guess:
-        print(f"Congratulations! {guess} is the secret number!")
-        print(f"You needed {attempts} attempts.")
-
-        score_list.append({"Name": username, "attempts": attempts, "date": str(datetime.datetime.now()),
-                           "wrong_guesses": wrong_guesses})
-
-        with open("resources/score_list.json", "w") as score_file:
-            score_file.write(json.dumps(score_list))
-        break
-    elif secret > guess:
-        wrong_guesses.append(guess)
-        print("The secret number is bigger. Try again.")
+    if selection.upper() == "A":
+        play_game()
+    elif selection.upper() == "B":
+        get_top_score()
     else:
-        wrong_guesses.append(guess)
-        print("The secret number is smaller. Try Again")
-
-print('Game over')
+        print('Game over')
+        break
