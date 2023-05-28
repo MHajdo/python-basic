1ac4b06440c8c45ab75a83b553ef5f01b3780a85
Matej Hajdinjak <hajdinjak.matej@gmail.com>
2023-05-28 10:47:07 +0200

    added play_game, get_country and get_capital functions, to make the game more re-playable

--- geography_quiz.py
+++ geography_quiz.py
@@ -1,16 +1,44 @@
 import json
 import random
 
-with open("resources/countries.json") as countries_file:
-    countries_cities = json.loads(countries_file.read())
 
-country, capital = random.choice(list(countries_cities.items()))
+def play_game():
+    country = get_country()
+    capital = get_capital(country)
+
+    while True:
+        answer = input(f"What is the capital city of {country}? ")
+
+        if answer.upper() == capital.upper():
+            print(f"Correct! {capital} is the capital city of {country}.")
+            break
+        else:
+            print(f"Wrong. {answer} is not the capital city of {country}. Try again.")
+
+
+def get_country():
+    with open("resources/countries.json") as countries_file:
+        countries_cities = json.loads(countries_file.read())
+
+        country = random.choice(list(countries_cities))
+
+        return country
+
+
+def get_capital(country):
+    with open("resources/countries.json") as countries_file:
+        countries_cities = json.loads(countries_file.read())
+
+        capital = countries_cities.get(country)
+
+        return capital
+
 
 while True:
-    answer = input(f"What is the capital city of {country}? ")
+    selection = input("Would you like to?\nA) Play a new game,\nB) Quit? ")
 
-    if answer.upper() == capital.upper():
-        print(f"Correct! {capital} is the capital city of {country}.")
+    if selection.upper() == "A":
+        play_game()
+    else:
+        print("Game over")
         break
-    elif answer != capital:
-        print(f"Wrong. {answer} is not the capital city of {country}. Try again.")
