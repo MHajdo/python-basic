4b0001189975d94a785ec50e43814f0929756009
Matej Hajdinjak <hajdinjak.matej@gmail.com>
2023-05-31 22:24:33 +0200

    added a model named Result

--- secret-number.py
+++ secret-number.py
@@ -3,10 +3,12 @@ import random
 import datetime
 
 
-def get_score_list():
-    with open("resources/score_list.json") as score_file:
-        score_list = json.loads(score_file.read())
-        return score_list
+class Result:
+    def __init__(self, username, attempts, date, wrong_guesses):
+        self.username = username
+        self.attempts = attempts
+        self.date = date
+        self.wrong_guesses = wrong_guesses
 
 
 def play_game(level):
@@ -22,14 +24,16 @@ def play_game(level):
         attempts += 1
 
         if secret == guess:
-            print(f"Congratulations! {guess} is the secret number!")
-            print(f"You needed {attempts} attempts.")
+            new_score = Result(username=username, attempts=attempts, date=str(datetime.datetime.now()),
+                               wrong_guesses=wrong_guesses)
 
-            score_list.append({"name": username, "attempts": attempts, "date": str(datetime.datetime.now()),
-                              "wrong_guesses": wrong_guesses})
+            score_list.append(new_score.__dict__)
 
             with open("resources/score_list.json", "w") as score_file:
                 score_file.write(json.dumps(score_list))
+
+            print(f"Congratulations! {guess} is the secret number!")
+            print(f"You needed {attempts} attempts.")
             break
         elif secret > guess and level == "easy":
             wrong_guesses.append(guess)
@@ -42,13 +46,26 @@ def play_game(level):
             print(f"{guess} is not the secret number. Try Again.")
 
 
+def get_score_list():
+    try:
+        with open("resources/score_list.json") as score_file:
+            score_list = json.loads(score_file.read())
+    except (FileNotFoundError, json.JSONDecodeError):
+        score_list = []
+
+    return score_list
+
+
 def get_top_score():
     score_list = get_score_list()
     score_list.sort(key=lambda d: d['attempts'])
 
     for score_dict in score_list[:3]:
-        print(f'Best scores: {score_dict["name"]}, {score_dict["attempts"]} attempts, date: {score_dict["date"]} '
-              f'unsuccessful guesses: {score_dict.get("wrong_guesses")}')
+        new_score = Result(username=score_dict.get("username"), attempts=score_dict.get("attempts"),
+                           date=score_dict.get("date"), wrong_guesses=score_dict.get("wrong_guesses"))
+
+        print(f"Name: {new_score.username}, attempts: {new_score.attempts}, date: {new_score.date},"
+              f" wrong guesses: {new_score.wrong_guesses}")
 
 
 while True:
